a6e55cd6b2d73cc30f16acd646908a17bc343316
Kevin Tandavo <riyuze211001@gmail.com>
2022-11-13 00:16:37 +0700

    Finished NavBar

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import React from 'react';
 import Cart from './Cart';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 class Homepage extends React.Component {
@@ -64,6 +70,33 @@ class Homepage extends React.Component {
             this.state.page === "Cart" ?
             <Cart changePage={this.changePage} cart={this.state.cart} /> :
             <div className="Homepage">
+                <Navbar bg="light" expand="lg">
+                    <Container fluid>
+                        <Navbar.Brand>Reacto E-Commerce</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Button variant="info me-2" onClick={this.cart}>Cart</Button>
+                            <Button variant="warning ms-2" onClick={this.logout}>Logout</Button>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                            value={this.state.item}
+                            onChange={e => this.setState({ item: e.target.value, items_filtered: [] })} 
+                            />
+                            <Button variant="success" onClick={this.findItems}>Search</Button>
+                        </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
                 
             </div>
         );
