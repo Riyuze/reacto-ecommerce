95713579556cbb34a9d106c57d84c506b6a44c23
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-10 14:40:05 +0700

    Modified search function

--- src/Homepage.js
+++ src/Homepage.js
@@ -101,7 +101,7 @@ class Homepage extends React.Component {
 
                 <Row xs={1} sm={2} md={3} lg={4} className="g-4">
                 {
-                    this.state.item === "" ?
+                    this.state.items_filtered == "" ?
                         this.state.items.map((item) => {
                             return (
                                 <Col>
