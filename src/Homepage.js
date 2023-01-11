e03b3e66525948f7452f56f48f1d7d54027591bd
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-11 12:00:54 +0700

    Added logo

--- src/Homepage.js
+++ src/Homepage.js
@@ -81,7 +81,11 @@ class Homepage extends React.Component {
 
                 <Navbar bg="black" variant="dark" expand="lg">
                     <Container fluid>
-                        <Navbar.Brand className="me-4 ms-2">Reacto</Navbar.Brand>
+                        <Navbar.Brand className="me-4 ms-2 d-flex">
+                            Reacto <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-bar-expand" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M3.646 10.146a.5.5 0 0 1 .708 0L8 13.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-4.292a.5.5 0 0 0 .708 0L8 2.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zM1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8z"/>
+                            </svg>
+                        </Navbar.Brand>
                             <Form className="d-flex w-100 ms-4">
                                 <Form.Control
                                 type="search"
