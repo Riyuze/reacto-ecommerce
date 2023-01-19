955bc4f70cb0ef6475a84441b4f693cb05b1bd55
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-19 12:18:12 +0700

    Added format currency function

--- src/Homepage.js
+++ src/Homepage.js
@@ -253,6 +253,13 @@ class Homepage extends React.Component {
         })
     }
 
+    formatCurrency = (price) => {
+        return new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR"
+        }).format(price);
+    }
+
     render() {
         return (
             <div className="Homepage bg-dark">
@@ -347,7 +354,7 @@ class Homepage extends React.Component {
                                                     <Card.Title>{item.name}</Card.Title>
                                                     <Card.Text>{item.detail}</Card.Text>
                                                     <div className="mt-auto d-flex flex-column">
-                                                        <Card.Text><h4><Badge bg="dark">Rp. {item.price}</Badge></h4></Card.Text>
+                                                        <Card.Text><Badge bg="dark" className="fs-5 fw-bold">{this.formatCurrency(item.price)}</Badge></Card.Text>
                                                         {
                                                             this.state.cart.find(cart => (cart.item === item)) ?   
                                                                 <div className="d-flex">                                                       
