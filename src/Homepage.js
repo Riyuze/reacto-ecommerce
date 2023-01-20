261561549aa8c95a6ec98b3a4165edab5935f5aa
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-20 16:49:08 +0700

    Started making navbar responsive

--- src/Homepage.js
+++ src/Homepage.js
@@ -289,6 +289,8 @@ class Homepage extends React.Component {
                                 <path fill-rule="evenodd" d="M3.646 10.146a.5.5 0 0 1 .708 0L8 13.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-4.292a.5.5 0 0 0 .708 0L8 2.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zM1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8z"/>
                             </svg>
                         </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
                             <Form className="d-flex w-100 ms-4">
                                 <Form.Control
                                 type="search"
@@ -325,6 +327,7 @@ class Homepage extends React.Component {
                                     <Button variant="danger" className="ms-1 me-2 text-white" onClick={this.logout}>Logout</Button>        
                                 </div>                       
                             }
+                            </Navbar.Collapse>
                     </Container>
                 </Navbar>
 
