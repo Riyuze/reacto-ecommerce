fd93b45919d064f33255a68dfaee08a7c4ab7908
Kevin Tandavo <riyuze211001@gmail.com>
2023-01-24 11:51:33 +0700

    Updates

---
+++ precache-manifest.65cfc66c16707f47dd77e1a1029d7078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "841acccd3f2121fcbb70",
+    "url": "/ecommerce-react/static/css/main.4f27842d.chunk.css"
+  },
+  {
+    "revision": "841acccd3f2121fcbb70",
+    "url": "/ecommerce-react/static/js/main.841acccd.chunk.js"
+  },
+  {
+    "revision": "a68d3ba9bd26c104b9c8",
+    "url": "/ecommerce-react/static/js/1.a68d3ba9.chunk.js"
+  },
+  {
+    "revision": "a7cf928ead2e8b219d48",
+    "url": "/ecommerce-react/static/js/runtime~main.a7cf928e.js"
+  },
+  {
+    "revision": "e2fe85657150e06f96fd3c0005df0273",
+    "url": "/ecommerce-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ae914110cf8f929b067c44e9e3fd0ae.js"
+  "/ecommerce-react/precache-manifest.65cfc66c16707f47dd77e1a1029d7078.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ecommerce-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
